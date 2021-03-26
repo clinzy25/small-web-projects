582feffa2d34d634b835cd7c2de04ec0e29763c7
clinzy25 <conner.linzy25@gmail.com>
2021-03-26 14:59:06 -0400

    added content

---
+++ background-generator/background.js
@@ -0,0 +1,35 @@
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var css = document.querySelector('h3');
+var body = document.getElementById("gradient");
+var random = document.getElementById("random");
+
+function setGradient() {
+  body.style.background =
+    "linear-gradient(to right, "
+    + color1.value
+    + ", "
+    + color2.value
+    + ")";
+
+    css.textContent = body.style.background + ";";
+}
+
+function randomGenerator() {
+  return (Math.floor(100000 + Math.random() * 900000));
+}
+
+function randomGradient() {
+  body.style.background =
+	"linear-gradient(to right, "
+	+ "#" + randomGenerator()
+	+ ", "
+	+ "#" + randomGenerator()
+	+")";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+random.addEventListener("click", randomGradient);
