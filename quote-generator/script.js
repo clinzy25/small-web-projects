a81dcdef488952e6ba07d5ed1384d666861bde20
clinzy25 <conner.linzy25@gmail.com>
2021-03-30 17:14:45 -0400

    added eslint

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,9 +1,9 @@
-const quoteContainter = document.querySelector("#quote-container");
-const quoteText = document.querySelector("#quote");
-const authorText = document.querySelector("#author");
-const twitterBtn = document.querySelector("#twitter");
-const newQuoteBtn = document.querySelector("#new-quote");
-const loader = document.querySelector("#loader");
+const quoteContainter = document.querySelector('#quote-container');
+const quoteText = document.querySelector('#quote');
+const authorText = document.querySelector('#author');
+const twitterBtn = document.querySelector('#twitter');
+const newQuoteBtn = document.querySelector('#new-quote');
+const loader = document.querySelector('#loader');
 
 /**
  * @var{array} apiQuotes container for quote fetch populated on-load, once only.
@@ -11,13 +11,13 @@ const loader = document.querySelector("#loader");
 let apiQuotes = [];
 
 function showLoadingSpinner() {
-  loader.hidden = false;
-  quoteContainter.hidden = true;
+    loader.hidden = false;
+    quoteContainter.hidden = true;
 }
 
 function removeLoadingSpinner() {
-  quoteContainter.hidden = false;
-  loader.hidden = true;
+    quoteContainter.hidden = false;
+    loader.hidden = true;
 }
 
 /**
@@ -25,41 +25,41 @@ function removeLoadingSpinner() {
  * Populate @element quoteContainer with relevant data
  */
 function newQuote() {
-  showLoadingSpinner();
-  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  if (!quote.author) {
-    authorText.textContent = "- Unknown";
-  } else {
-    authorText.textContent = `- ${quote.author}`;
-  }
-  if (quote.text.length > 120) {
-    quoteText.classList.add("long-quote");
-  } else {
-    quoteText.classList.remove("long-quote");
-  }
-  quoteText.textContent = quote.text;
-  removeLoadingSpinner();
+    showLoadingSpinner();
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    if (!quote.author) {
+        authorText.textContent = '- Unknown';
+    } else {
+        authorText.textContent = `- ${quote.author}`;
+    }
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
+    removeLoadingSpinner();
 }
 
 async function getQuotes() {
-  showLoadingSpinner();
-  const apiUrl = "https://type.fit/api/quotes";
-  try {
-    const response = await fetch(apiUrl);
-    apiQuotes = await response.json();
-    newQuote();
-  } catch (error) {
-    alert("There was a problem fetching the quote");
-  }
+    showLoadingSpinner();
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        alert('There was a problem fetching the quote');
+    }
 }
 
 function tweetQuote() {
-  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
-  window.open(twitterUrl, "_blank");
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
 }
 
-newQuoteBtn.addEventListener("click", newQuote);
-twitterBtn.addEventListener("click", tweetQuote);
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 /**
  * On-load
