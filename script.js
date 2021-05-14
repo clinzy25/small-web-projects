33fdbbf86dbc951e671c7454c5dbb079b5a40028
clinzy25 <conner.linzy25@gmail.com>
2021-05-14 17:15:13 -0400

    added home animations

---
+++ script.js
@@ -0,0 +1,6 @@
+AOS.init({
+    delay: 200, // values from 0 to 3000, with step 50ms
+    duration: 1200, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    mirror: false, // whether elements should animate out while scrolling past them
+});
